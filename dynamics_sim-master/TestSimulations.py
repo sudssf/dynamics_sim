d221f6e4746486917d5e7e0fb7b0baaf4712f85a
Anjalika Nande <anande91@gmail.com>
2020-05-27 23:23:49 -0400

    cleaned up the test simulations file

--- dynamics_sim-master/TestSimulations.py
+++ dynamics_sim-master/TestSimulations.py
@@ -1,8 +1,7 @@
+# Testing main functions using the Hawk Dove and Hawk Dove Bourgeois games
+
 from wrapper import GameDynamicsWrapper, VariedGame
-
 from dynamics.wright_fisher import WrightFisher
-
-from games.example_games.coordination import Coordination
 from games.example_games.hawk_dove import HawkDove
 from games.example_games.hdb import HawkDoveBourgeois
 
@@ -16,27 +15,15 @@ class TestCase(unittest.TestCase):
 
     def test_single_simulation(self):
         s = GameDynamicsWrapper(HawkDove, WrightFisher)
-        s.simulate(num_gens=100, graph=dict(shading='redblue', options=['area', 'meanStratLine', 'payoffLine', 'smallFont']))
-
-    def test_vary_one(self):  # Simulates while changing a single variable over time
-        s = VariedGame(Coordination, WrightFisher, dynamics_kwargs=dict(uniDist=True))
-        s.vary_param('a', (0, 100, 5), num_gens=500, num_iterations=5, graph=dict(options=['area']))
+        s.simulate(num_gens=100, graph=dict(options=['area', 'smallFont']))
 
     def test_many_simulation(self):  # Determines which equilibria result based upon several simulations, text output
         s = GameDynamicsWrapper(HawkDove, WrightFisher)
         print(s.simulate_many(num_iterations=100, num_gens=100, graph=dict(options=['area', 'smallFont'])))
 
-if __name__ == '__main__':
-    unittest.main()
-
-if False:
-    def calculate_stationary(self):
-        s = GameDynamicsWrapper(Coordination, WrightFisher)
-        s.stationaryDistribution()
-
-    def test_vary_one2(self):  # Simulates while changing a single variable over time
-        s = VariedGame(HawkDove, WrightFisher, dynamics_kwargs=dict(uniDist=True))
-        s.vary_param('high_sender_proportion', (1, 15, 37), num_gens=160, num_iterations=500, graph=dict(area=True, normalize=9, lineArray=[(.36, .36, 0, 1)]))
+    def test_vary_one(self):  # Simulates while changing a single variable over time
+        s = VariedGame(HawkDoveBourgeois, WrightFisher, dynamics_kwargs=dict(uniDist=True))
+        s.vary_param('v', (0, 5, 1), num_gens=500, num_iterations=5, graph=dict(options=['area']))
 
     def test_wireFrame(self):  # 3d graph of equilibrium found when varying two variables
         s = VariedGame(HawkDoveBourgeois, WrightFisher)
@@ -45,3 +32,6 @@ if False:
     def test_contour_graph(self):  # 2d contour color plot
         s = VariedGame(HawkDoveBourgeois, WrightFisher)
         s.vary_2params('v', (0, 50, 1), 'c', (0, 100, 1), num_iterations=1, num_gens=500, burn=499, graph=dict(type='contour', lineArray=[(0, 50, 0, 50)]))
+
+if __name__ == '__main__':
+    unittest.main()
