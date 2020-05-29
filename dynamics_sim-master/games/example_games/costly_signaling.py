d746ec978741d7e03f8d1c153c8788cda876c67b
Anjalika Nande <anande91@gmail.com>
2020-05-28 21:48:33 -0400

    Added more information regarding the games

--- dynamics_sim-master/games/example_games/costly_signaling.py
+++ dynamics_sim-master/games/example_games/costly_signaling.py
@@ -2,6 +2,8 @@ from games.game import Game
 import numpy as np
 
 class CostlySignaling(Game):
+    """ Class that defines a version of the costly signaling game. See : Hoffman, Moshe and Erez Yoeli, Costly Signaling," University Lecture 2015.
+    """
     DEFAULT_PARAMS = dict(lCost=3, hCost=1, a=5, aHigh=10, aLow=-10, lProp=2, hProp=1, bias_strength=0)
     PLAYER_LABELS = ('Low Sender', 'High Sender', 'Receiver')
     STRATEGY_LABELS = (('No', 'Low', 'Medium', 'High'),
@@ -13,7 +15,7 @@ class CostlySignaling(Game):
         lProp, hProp = lProp/(lProp+hProp), hProp/(lProp+hProp)
         lProp /= 2
         hProp /= 2
-        
+
         payoff_matrix_p1 = [[[0 for x in range(5)] for x in range(4)] for x in range(4)]
         for i1, lowSenderStrat in enumerate(payoff_matrix_p1):
             for i2, highSenderStrat in enumerate(lowSenderStrat):
@@ -21,7 +23,7 @@ class CostlySignaling(Game):
                     if i1 >= i3:
                         payoff_matrix_p1[i1][i2][i3] += a
                     payoff_matrix_p1[i1][i2][i3] -= lCost * i1
-                   
+
         payoff_matrix_p2 = [[[0 for x in range(5)] for x in range(4)] for x in range(4)]
         for i1, lowSenderStrat in enumerate(payoff_matrix_p1):
             for i2, highSenderStrat in enumerate(lowSenderStrat):
@@ -29,7 +31,7 @@ class CostlySignaling(Game):
                     if i2 >= i3:
                         payoff_matrix_p2[i1][i2][i3] += a
                     payoff_matrix_p2[i1][i2][i3] -= hCost * i2
-        
+
         payoff_matrix_p3 = [[[0 for x in range(5)] for x in range(4)] for x in range(4)]
         for i1, lowSenderStrat in enumerate(payoff_matrix_p1):
             for i2, highSenderStrat in enumerate(lowSenderStrat):
@@ -38,15 +40,15 @@ class CostlySignaling(Game):
                         payoff_matrix_p3[i1][i2][i3] += aLow * lProp
                     if i2 >= i3:
                         payoff_matrix_p3[i1][i2][i3] += aHigh * hProp
-        
+
         payoff_matrix = [payoff_matrix_p1, payoff_matrix_p2, payoff_matrix_p3]
         player_dist = (lProp, hProp, 1/2)
         super(CostlySignaling, self).__init__(payoff_matrices=payoff_matrix, player_frequencies=player_dist, bias_strength=bias_strength, equilibrium_tolerance=equilibrium_tolerance)
-        
+
     @classmethod
     def classify(cls, params, state, tolerance):
         threshold = 1-tolerance
-        
+
         if state[2][4] >= threshold:
             return 0#Receiver rejects all
         elif state[2][0] >= threshold:
@@ -59,5 +61,3 @@ class CostlySignaling(Game):
             return 4#Simple separating
         else:
             return super(CostlySignaling, cls).classify(params, state, tolerance)
-
-            
--- dynamics_sim-master/games/example_games/cwolMultiple.py
+++ dynamics_sim-master/games/example_games/cwolMultiple.py
@@ -2,6 +2,8 @@ from games.game import game
 from games.example_games.payoff_matrics.cwol_multiple import p1Payoffs, p2Payoffs
 
 class CwolMultiple(game):
+    """ Cooperate without looking game with 4 players.
+    """
     DEFAULT_PARAMS = dict(a=2, b=2, cl=3, ch=5, dl=2.5, dh=5, p=.8, q=.7, r=.3, \
                           wl=.1, wh=.7, p1lProp=.7, p2lProp=.7)
     PLAYER_LABELS = ('Low Player 1', 'High Player 1', 'Low Player 2', 'High Player 2')
@@ -11,14 +13,13 @@ class CwolMultiple(game):
                        ('Exit', 'Exit Look', 'Exit Defect low', 'Exit Look Defect', 'Never Exit'))
     EQUILIBRIA_LABELS = ('Defect Exit', 'CWOL separating', 'Cooperate stay')
     #Listing low, then high
-    
+
     def __init__(self, a, b, cl, ch, dl, dh, p, q, r, wl, wh, p1lProp, p2lProp):
-        
+
         p1hProp = (1 - p1lProp) / 2
         p1lProp /= 2
-        
+
         p2hProp = (1 - p2lProp) /2
         p2lProp /= 2
-        
+
         payoff_1l = p1Payoffs()
-        
